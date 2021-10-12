2dbb5d7cd99c7d26ef754b62087be6808783ee34
Greg Zaragoza <greg.g.zaragoza@gmail.com>
2021-10-12 00:13:53 -0500

    Added routes for notes.html, db.json retrieval

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const path = require('path');
+
+const notes = require('./db/db.json');
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+app.get('/api/notes', (req, res) => {
+    // console.info(`GET /api/reviews`);
+    res.status(200).json(notes);
+  });
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
