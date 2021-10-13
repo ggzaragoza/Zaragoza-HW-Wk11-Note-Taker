3aa60a63c72e65d40612ffc823e2c9bee967d46c
Greg Zaragoza <greg.g.zaragoza@gmail.com>
2021-10-13 17:03:36 -0500

    Notes are added to sidebar via updated request

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const fs = require('fs');
 
 const notes = require('./db/db.json');
 const noteId = require('./helpers/noteid.js');
+// const index = require('./public/assets/js/index.js');
 
 
 const PORT = process.env.PORT || 3001;
@@ -35,7 +36,7 @@ app.get('/notes', (req, res) =>
 
 
 app.post('/api/notes', (req, res) => {
-  console.info(`${req.method} request received to add a review.`);
+  console.info(`${req.method} request received to add a note.`);
 
   const { title, text } = req.body;
 
@@ -56,6 +57,10 @@ app.post('/api/notes', (req, res) => {
         
         fs.writeFile('./db/db.json', JSON.stringify(parsedNotes, null, 1), (err) =>
           err ? console.error(err) : console.info('Your new note was successfully saved!'))
+
+            app.get('/api/notes', (req, res) => 
+    res.status(200).json(notes)
+);
       };
     });
 
@@ -68,20 +73,114 @@ app.post('/api/notes', (req, res) => {
     res.status(201).json(response);
 
   } else {
-    res.status(500).json('Error in posting review.');
+    res.status(500).json('Error in posting note.');
   }
 
-});
+  
+
+})
 
 
 app.get('/api/notes', (req, res) => 
-    res.status(200).json(notes)
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+      res.json(JSON.parse(data))
+    }
+    )
+    // res.status(200).json(notes)
 );
-
 
 app.get('/api/notes/:id', (req, res) => 
     res.status(200).json(notes)
 );
+
+// app.post('/api/notes/:id', (req, res) => 
+//     res.status(200).json(notes)
+// );
+
+app.delete('/api/notes/:id', (req, res) => {
+  console.info(`${req.method} request received to delete a note.`);
+
+  // const currentNoteId = notes[i].id;
+  // const result = [];
+  const idtoDelete = req.params.id;
+  console.log(idtoDelete);
+
+  if (idtoDelete) {
+    // const newNotes = [];
+
+    // for (let i = 0; i < notes.length; i++) {
+    //     const currentNoteId = notes[i].id;
+
+    //     if (idtoDelete === currentNoteId) {
+    //       const result = notes.filter(() => notes.indexOf(i));
+    //       newNotes.push(result);
+    //       // return newNotes;
+    //     }
+    // }
+
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+      } else {
+        const parsedNotes = JSON.parse(data);
+
+        for (let i = 0; i < parsedNotes.length; i++) {
+          // let existingNoteId = parsedNotes[i].id;
+  
+          if (idtoDelete === parsedNotes[i].id) {
+            const newNotes = parsedNotes.filter((item, i) => parsedNotes.indexOf(item !== i));
+
+            fs.writeFile('./db/db.json', JSON.stringify(newNotes, null, 1), (err) =>
+            err ? console.error(err) : console.info('Your note has been deleted.'))
+          }
+
+        break;
+        }
+      
+
+        // return newNotes;
+    
+        // console.info(newNotes);
+      }
+    })
+
+  
+
+    // fs.readFile('./db/db.json', 'utf8', (err, data) => {
+    //   if (err) {
+    //     console.error(err)
+    //   } else {
+    //     const parsedNotes = JSON.parse(data);
+
+    //     parsedNotes.push(newNotes);
+        
+    //     fs.writeFile('./db/db.json', JSON.stringify(parsedNotes, null, 1), (err) =>
+    //       err ? console.error(err) : console.info('Your note has been deleted.'))
+    //   };
+    // });
+
+    // const newNote = {
+    //   title,
+    //   text,
+    //   id: noteId(),
+    // };
+
+    
+
+    const response = {
+      status: 'success',
+      // body: newNotes
+    };
+  
+    console.log(response);
+    res.status(201).json(response);
+
+  } else {
+    res.status(500).json('Error in deleting note.');
+  }
+
+})
+
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
